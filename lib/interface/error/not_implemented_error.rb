241f8b9461f97f1a8d7490a64101fee2a7ee4f1e
rodrigo-ehresmann <igoehresmann@gmail.com>
2016-09-02 00:42:21 -0300

    Implement 'must_implement_without_arity'

--- lib/interface/error/not_implemented_error.rb
+++ lib/interface/error/not_implemented_error.rb
@@ -1,16 +1,17 @@
 module Interface
   module Error
-    # Raised when a method has not been implemented by a class that
-    # has used the implements <InterfaceName> method.
+    # Raised when a method has not been implemented (or implemented without
+    # especified arity) by a class that has used the implements <InterfaceName> method.
     class NotImplementedError < NoMethodError
-      def initialize(class_name:, method_name:, interface_name:)
-        super(error_message(class_name, method_name, interface_name), method_name)
+      def initialize(info = {})
+        super(error_message(info), info[:method_name])
       end
 
       private
 
-      def error_message(class_name, method_name, interface_name)
-        "Expected #{class_name} to implement #{method_name} for interface #{interface_name}"
+      def error_message(info = {})
+        info[:method_name] = "#{info[:method_name]}/#{info[:method_arity]}" if !info[:method_arity].nil?
+        "Expected #{info[:class_name]} to implement #{info[:method_name]} for interface #{info[:interface_name]}"
       end
     end
   end
--- lib/interface/helpers.rb
+++ lib/interface/helpers.rb
@@ -14,5 +14,24 @@ module Interface
               )
       end
     end
+
+    def must_implement_with_arity(methods = {})
+      puts self
+      methods.each do |method_name, arity|
+        next if respond_to?(method_name) && method_arity(method_name) == arity
+        raise Interface::Error::NotImplementedError.new(
+                class_name: self.class.name,
+                method_name: method_name,
+                method_arity: arity,
+                interface_name: self.class.ancestors.first
+              )
+      end
+    end
+
+    private
+
+    def method_arity(method_name)
+      method(method_name).arity.abs
+    end
   end
 end
