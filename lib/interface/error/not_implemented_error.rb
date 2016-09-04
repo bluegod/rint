c7b3393b162ade3ac095f4c3a0eccaa1bbc53e02
rodrigo-ehresmann <igoehresmann@gmail.com>
2016-09-04 14:08:21 -0300

    Remove 'must_implement_with_arity' and transfer its functionalities to 'must_implement' method

--- lib/interface/error/not_implemented_error.rb
+++ lib/interface/error/not_implemented_error.rb
@@ -1,17 +1,16 @@
 module Interface
   module Error
-    # Raised when a method has not been implemented (or implemented without
-    # especified arity) by a class that has used the implements <InterfaceName> method.
+    # Raised when a method has not been implemented by a class that
+    # has used the implements <InterfaceName> method.
     class NotImplementedError < NoMethodError
-      def initialize(info = {})
-        super(error_message(info), info[:method_name])
+      def initialize(class_name:, method_name:, method_arity:, interface_name:)
+        super(error_message(class_name, method_name, method_arity, interface_name), method_name)
       end
 
       private
 
-      def error_message(info = {})
-        info[:method_name] = "#{info[:method_name]}/#{info[:method_arity]}" if !info[:method_arity].nil?
-        "Expected #{info[:class_name]} to implement #{info[:method_name]} for interface #{info[:interface_name]}"
+      def error_message(class_name, method_name, method_arity, interface_name)
+        "Expected #{class_name} to implement #{method_name}/#{method_arity} for interface #{interface_name}"
       end
     end
   end
--- lib/interface/helpers.rb
+++ lib/interface/helpers.rb
@@ -2,27 +2,18 @@ require 'interface/error/not_implemented_error'
 
 module Interface
   module Helpers
-    # Errors raised here identify the class_name that is not implementing
-    # the method required by the Interface.
+    # Errors raised here identify the class_name that is not implementing or is
+    # implemented with the wrong arity required by the Interface.
     def must_implement(*args)
-      args.each do |method_name|
-        next if respond_to? method_name
+      args.each do |method|
+        method = {method.to_sym => 0} if !method.is_a? Hash # If it isn't specified any arity, it's assumed like 0.
+        method_name = method.keys.first
+        method_arity = method.values.first
+        next if respond_to?(method_name) && method_arity(method_name) == method_arity
         raise Interface::Error::NotImplementedError.new(
                 class_name: self.class.name,
                 method_name: method_name,
-                interface_name: self.class.ancestors.first
-              )
-      end
-    end
-
-    def must_implement_with_arity(methods = {})
-      puts self
-      methods.each do |method_name, arity|
-        next if respond_to?(method_name) && method_arity(method_name) == arity
-        raise Interface::Error::NotImplementedError.new(
-                class_name: self.class.name,
-                method_name: method_name,
-                method_arity: arity,
+                method_arity: method_arity,
                 interface_name: self.class.ancestors.first
               )
       end
@@ -30,6 +21,7 @@ module Interface
 
     private
 
+    # Arity of the implemented method.
     def method_arity(method_name)
       method(method_name).arity.abs
     end
