3cc355c1e41ef390c77b4489ee161df7b84344e7
rodrigo-ehresmann <igoehresmann@gmail.com>
2016-09-06 12:51:50 -0300

    Fix treatment of hash arguments bug

--- lib/interface/helpers.rb
+++ lib/interface/helpers.rb
@@ -6,31 +6,38 @@ module Interface
     # implemented with the wrong arity required by the Interface.
     def must_implement(*args)
       args.each do |method|
-        method = method_info(method)
-        next if respond_to?(method[:name]) && expected_arity?(method)
-        raise Interface::Error::NotImplementedError.new(
-                class_name: self.class.name,
-                method_name: method[:name],
-                method_arity: method[:arity],
-                interface_name: self.class.ancestors.first
-              )
+        if method.is_a? Hash
+          method.each do |name, arity|
+            next if valid_method?(name, arity)
+            raise_interface_error(name, arity)
+          end
+        else
+          next if valid_method?(method)
+          raise_interface_error(method)
+        end
       end
     end
 
     private
 
-    # Build a hash with the method name and arity.
-    def method_info(method)
-      if method.is_a? Hash
-        { name: method.keys.first, arity: method.values.first }
-      else
-        { name: method, arity: nil }
-      end
+    def raise_interface_error(method_name, method_arity = nil)
+      raise Interface::Error::NotImplementedError.new(
+              class_name: self.class.name,
+              method_name: method_name,
+              method_arity: method_arity,
+              interface_name: self.class.ancestors.first
+            )
     end
 
-    def expected_arity?(method)
-      return true unless method[:arity]
-      method_arity(method[:name]) == method[:arity]
+    # Check if method is implemented and have the specified arity (where nil
+    # points to 'unlimited').
+    def valid_method?(name, arity = nil)
+      respond_to?(name) && expected_arity?(name, arity)
+    end
+
+    def expected_arity?(method, arity)
+      return true unless arity
+      method_arity(method) == arity
     end
 
     # Arity of the implemented method.
