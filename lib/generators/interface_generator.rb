88af344e223e2968a514ff60e5f92dbebc351522
rodrigo-ehresmann <igoehresmann@gmail.com>
2016-09-06 00:28:51 -0300

    Update generator

--- lib/generators/interface_generator.rb
+++ lib/generators/interface_generator.rb
@@ -35,16 +35,13 @@ class InterfaceGenerator < Thor::Group
   end
 
   def methods
-    methods = args[2..-1].map do |m|
-                if m.scan(/\d+/).empty?
-                  ":#{m}"
-                else
-                  m_copy = m.dup
-                  m_copy[":"] = ": " # Create and modify a copy because 'm' is frozen in runtime.
-                  m_copy
-                end
-              end
-    methods.join(", ")
+    args[2..-1].map do |m|
+      if m.scan(/\d+/).empty?
+        ":#{m}"
+      else
+        m.gsub(":", ": ")
+      end
+    end.join(", ")
   end
 
   def name
